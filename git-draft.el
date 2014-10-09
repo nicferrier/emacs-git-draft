6d083b90f39519f43fefe211cac1c43f8a5ab902
Nic Ferrier <nferrier@ferrier.me.uk>
2014-10-09 16:29:56 +0100

    format as a package

--- git-draft.el
+++ git-draft.el
@@ -1,3 +1,46 @@
+;;; git-draft.el  -- draft git commit messages -*- lexical-binding: t -*-
+
+;; Copyright (C) 2014  Nic Ferrier
+
+;; Author: Nic Ferrier <nferrier@ferrier.me.uk>
+;; Keywords: processes
+;; Version: 0.0.1
+;; Package-requires: ((dash "2.9.0"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This let's you make a draft of what has changed in your git project
+;; for inserting into commit messages.  In your commit buffer do:
+
+;;   M-x git-draft
+
+;; and git-draft will analyze your changes and try and pull out
+;; function names that have changed.
+
+;; It probably only works for EmacsLisp and similar languages.  And
+;; it's just not very good anyway.  But it's a start.  Maybe.
+
+;; == Obvious todos
+
+;; We clearly need to be able to install into a hook for git commit,
+;; either magit's or vc's.  I guess that should be configurable or
+;; something.
+
+
+;;; Code:
 
 (require 'rx)
 (require 's)
